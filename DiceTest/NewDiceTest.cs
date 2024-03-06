5b71bdab024c9c3f74147d19562d16f2128b3cb2
Karol <dopierala.karol@gmail.com>
2024-03-06 20:08:44 +0100

    Change in DiceTest

--- DiceTest/NewDiceTest.cs
+++ DiceTest/NewDiceTest.cs
@@ -6,17 +6,24 @@ namespace DiceTest
     [TestFixture]
     class NewDiceTest
     {
+        private NewDice _dice;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _dice = new NewDice(6);
+        }
+
         [Test, Order(0)]
         public void NewDice_CheckIfObjectAreCreatedWithCorrectID_returnCorrectID()
         {
             //Arrange
-            var dice1 = new NewDice(1);
             var dice2 = new NewDice(2);
             var dice3 = new NewDice(3);
             var dice4 = new NewDice(4);
 
             //Act
-            int idDice1 = dice1.Id;
+            int idDice1 = _dice.Id;
             int idDice2 = dice2.Id;
             int idDice3 = dice3.Id;
             int idDice4 = dice4.Id;
@@ -31,12 +38,8 @@ namespace DiceTest
         [Test]
         public void NewDice_CheckIfConstructorCreateCorrectObject_ReturnWallNo()
         {
-            //Arrange
-            var dice = new NewDice(6);
-
             //Act
-            int noOfWall = dice.HowManyWall;
-            dice = null;
+            int noOfWall = _dice.HowManyWall;
 
             //Assert
             Assert.AreEqual(6, noOfWall);
